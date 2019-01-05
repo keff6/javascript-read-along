fc7862cd03cddf7245d2672001c9c443656ac3b3
keff6 <kev.fallas@gmail.com>
2019-01-05 12:14:25 -0600

    Add distribuition of area

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
-window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+(function (document, window) {
+    console.log(window)
+    window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
-const recognition =  new SpeechRecognition();
-recognition.interimResults = true;
+    const recognition =  new SpeechRecognition();
+    recognition.interimResults = true;
 
-let p = document.createElement('p');
-const words = document.querySelector('.words');
-words.appendChild(p);
-
-recognition.addEventListener('result', (e) => {
-const transcript = Array.from(e.results)
-    .map(result => result[0])
-    .map(result => result.transcript)
-    .join('');
-
-    p.textContent = transcript;
-    if (e.results[0].isFinal) {
-    p = document.createElement('p');
+    let p = document.createElement('p');
+    const words = document.querySelector('.talk');
+    console.log(words)
     words.appendChild(p);
-    }
-});
 
-recognition.addEventListener('end', recognition.start);
+    recognition.addEventListener('result', (e) => {
+    const transcript = Array.from(e.results)
+        .map(result => result[0])
+        .map(result => result.transcript)
+        .join('');
 
-recognition.start();
+        p.textContent = transcript;
+        if (e.results[0].isFinal) {
+        p = document.createElement('p');
+        words.appendChild(p);
+        }
+    });
+
+    recognition.addEventListener('end', recognition.start);
+
+    recognition.start();
+})(document, window);
